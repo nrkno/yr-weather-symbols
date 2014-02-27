e24bef8055debdc85f5f5e6a5ffb8fe542d4721c
Alexander Pope <alex@pope-industries.com>
2014-02-27 08:46:37 +0100

    fix path

--- index.js
+++ index.js
@@ -4854,7 +4854,7 @@ require.register('weatherSymbol', function(module, exports, require) {
   					? CANVAS
   					: IMG)
   		, element = createElement(type)
-  		, id = container.getAttribute('data-id').split('.')[0]
+  		, id = options.id || container.getAttribute('data-id')
   		, w = container.offsetWidth
   		, h = container.offsetHeight
   		, scale = options.scale || 1
