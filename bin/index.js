0d8170eabc93014d1372e90d12c4739884f799e3
André Stokke <andre.stokke@nrk.no>
2022-05-11 09:44:50 +0200

    Copy map symbols from yr-web repo to shadows folder

--- bin/index.js
+++ bin/index.js
@@ -2,7 +2,7 @@ var convertSvgFiles = require('@nrk/yr-convert-graphics').convertSvgFiles;
 
 const pngSizes = [100, 200];
 const expectedSvgDimensionsDefault = { width: 100, height: 100 };
-const expectedSvgDimensionsShadows = { width: 124, height: 124 };
+const expectedSvgDimensionsShadows = { width: 130, height: 130 };
 
 convertSvgFiles({
   src: 'symbols/light-mode/default',
