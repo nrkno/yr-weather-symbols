cd64f5b9c4179be200150ad7b7c431058da64d1d
Andre Stokke <andre.stokke@nrk.no>
2023-09-26 13:32:22 +0200

    YR-6177: Update paths in index.js

--- bin/index.js
+++ bin/index.js
@@ -2,18 +2,18 @@ var convertSvgFiles = require('@nrk/yr-convert-graphics').convertSvgFiles;
 
 const pngSizes = [100, 200];
 const expectedSvgDimensionsDefault = { width: 100, height: 100 };
-const expectedSvgDimensionsShadows = { width: 130, height: 130 };
+const expectedSvgDimensionsDarkmode = { width: 130, height: 130 };
 
 convertSvgFiles({
-  src: 'symbols/light-mode/default',
+  src: 'symbols/default',
   pngSizes,
   expectedSvgDimensions: expectedSvgDimensionsDefault,
-  output: 'light-mode/default',
+  output: 'symbols/default',
 });
 
 convertSvgFiles({
-  src: 'symbols/light-mode/shadows',
+  src: 'symbols/darkmode',
   pngSizes,
-  expectedSvgDimensions: expectedSvgDimensionsShadows,
-  output: 'light-mode/shadows',
+  expectedSvgDimensions: expectedSvgDimensionsDarkmode,
+  output: 'symbols/darkmode',
 });
